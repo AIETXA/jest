136278b16e91bca1c9f92ccccdc134032371c320
Ailén <ailenechaguibel@gmail.com>
2025-05-20 12:17:34 +0200

    trabajo terminado

--- product.test.js
+++ product.test.js
@@ -3,11 +3,12 @@ const { resetProducts,  addProduct, removeProduct, getProducts, getProduct, upda
 
 describe ('add products', () => {
     beforeEach(() => {
-      products.length = 0; 
+    resetProducts();
     });
-it('add products ok', () => {
+
+    it('add products ok', () => {
  const result = addProduct('Cafetera', 120);
- expect(result).toEqual({name: 'Cafetera', price: 120 });
+ expect(result).toEqual({id: 0, name: 'Cafetera', price: 120 });
 });
 it('throw error if name is missing', () => {
     expect(() => addProduct(undefined, 120)).toThrow('The name parameter is missing');
@@ -20,4 +21,50 @@ it('throw an error if the product already exists', () => {
     expect(() => addProduct('Tv', 300)).toThrow('The product already exists');
 });
 
+});
+
+describe('remove product', () => {
+     beforeEach(() => {
+    resetProducts(); 
+     });
+  it('throw and error if the id not found', () => {
+    expect(() => removeProduct('id-inexistente')).toThrow('Product not found'); 
 })
+})
+
+
+describe('get product', () => {
+     beforeEach(() => {
+    resetProducts(); 
+     });
+    it('throw and error if the product not exist', () => {
+        expect(() => getProduct(99)).toThrow('Product not found');
+    })
+})
+
+
+describe('update product ok', () => {
+    beforeEach(() => {
+    resetProducts(); 
+    addProduct('Cafetera', 120)
+     })
+    
+    it('updates name and price correctly',() => {
+     const updated = updateProduct(0, 'Microondas', 200);
+     expect(updated).toEqual({id: 0, name: 'Microondas', price: 200 });
+    });
+
+    it('updates only name if price is undefined', () => {
+     const updated = updateProduct(0, 'Nintendo', undefined);
+     expect(updated).toEqual({id: 0, name: 'Nintendo', price: 120 });
+    })
+   
+    it('updates only price if name is undefined', () => {
+     const updated = updateProduct(0, undefined, 150);
+     expect(updated).toEqual({id: 0, name: 'Cafetera', price: 150 });
+    })
+   
+    it('Throw an error if product does not exists', () => {
+     expect(() => updateProduct(99, 'Plancha', 100)).toThrow('Product not found');
+    });
+});
--- productos/product.js
+++ productos/product.js
@@ -1,42 +1,69 @@
 let products = [] ;
-let id = 0;
-
+let currentId = 0;
 
 function resetProducts() {
-  products = [];
-  id = 0;
+  products.length = 0;
+  currentId = 0;
 }
 
-function getProducts() {
-  return products;
+function getProduct(id) {
+    const product = products.find(p => p.id === id);
+  if (!product) {
+    throw new Error('Product not found');
+  }
+  return product;
 }
 
 function addProduct(name, price) {
-    if(name === undefined || price === undefined) {
-        throw new Error('Missing mandatory parameters')
+    if(name === undefined) {
+        throw new Error('The name parameter is missing')
+    }
+    if(price === undefined) {
+      throw new Error('The price parameter is missing')
     }
 
+    const existe = products.some(p => p.name === name);
     if (existe) {
         throw new Error('The product already exists');
 }
 
-id++;
-let newProduct = { id, name, price };
+
+let newProduct = { 
+  id: currentId++,
+   name, 
+   price };
+
 products.push(newProduct);
-
-
+return newProduct;
+}
 
 
 
 function removeProduct(id) {
-    if(id === undefined)
-
+  const indice = products.findIndex(p => p.id === id);
+    if(indice === -1) {
+      throw new Error('Product not found');
+    }
+    products.splice(indice, 1);
 }
 
-module.exports = { products, id, 
-    getProducts,
+function updateProduct(id, name, price) {
+  const product = products.find(p => p.id === id);
+  if (!product) {
+    throw new Error('Product not found');
+  }
+
+  if (name !== undefined) product.name = name;
+  if (price !== undefined) product.price = price;
+
+  return product;
+}
+
+
+module.exports = { products, 
+    getProduct,
     addProduct,
     resetProducts,
     removeProduct, 
-
+    updateProduct,
 }
